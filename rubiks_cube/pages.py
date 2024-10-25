986300b3bff75b6dc2b190b52f11080003a5d4ac
Martin Gudahl Tufte <martingudahltufte@hotmail.com>
2024-10-25 23:30:33 +0200

    Fix import name

--- rubiks_cube/pages.py
+++ rubiks_cube/pages.py
@@ -12,7 +12,7 @@ from rubiks_cube.move.generator import MoveGenerator
 from rubiks_cube.solver import solve_step
 from rubiks_cube.state import get_rubiks_cube_state
 from rubiks_cube.state.utils import invert
-from rubiks_cube.tag.patterns import get_cubexes
+from rubiks_cube.tag.cubex import get_cubexes
 from rubiks_cube.utils.parsing import parse_scramble
 from rubiks_cube.utils.parsing import parse_user_input
 
