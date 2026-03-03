e1ada8dfbaa8b037b7c9726d55da5b930d19998c
martintufte <martingudahltufte@hotmail.com>
2026-03-03 18:13:44 +0100

    Move canonicalize_rotations to meta.py

--- rubiks_cube/move/meta.py
+++ rubiks_cube/move/meta.py
@@ -3,19 +3,66 @@ from __future__ import annotations
 from dataclasses import dataclass
 from functools import lru_cache
 from typing import TYPE_CHECKING
+from typing import Final
+from typing import Sequence
 
 from rubiks_cube.move.utils import is_rotation
 from rubiks_cube.representation.permutation import create_permutations
-from rubiks_cube.representation.rotation import canonicalize_rotations
+from rubiks_cube.representation.permutation import get_identity_permutation
 from rubiks_cube.representation.utils import get_identity
 
 if TYPE_CHECKING:
     from rubiks_cube.configuration.types import CubePermutation
 
 
+# State (X, Y) means original X face points Up and original Y face points Front
+# Canonical solution: 0/1 directly, or rotate top face correctly, then front face
+CANONICAL_ROTATION_SEQUENCES: Final[dict[tuple[int, int], list[str]]] = {
+    (0, 1): [],
+    (0, 2): ["y"],
+    (0, 3): ["y2"],
+    (0, 4): ["y'"],
+    (1, 0): ["x", "y2"],
+    (1, 2): ["x", "y"],
+    (1, 4): ["x", "y'"],
+    (1, 5): ["x"],
+    (2, 0): ["z'", "y'"],
+    (2, 1): ["z'"],
+    (2, 3): ["z'", "y2"],
+    (2, 5): ["z'", "y"],
+    (3, 0): ["x'"],
+    (3, 2): ["x'", "y"],
+    (3, 4): ["x'", "y'"],
+    (3, 5): ["x'", "y2"],
+    (4, 0): ["z", "y"],
+    (4, 1): ["z"],
+    (4, 3): ["z", "y2"],
+    (4, 5): ["z", "y'"],
+    (5, 1): ["z2"],
+    (5, 2): ["x2", "y"],
+    (5, 3): ["x2"],
+    (5, 4): ["x2", "y'"],
+}
+
+
+# TODO(martin): Consider implementing the full Cayley table
+def canonicalize_rotations(rotations: Sequence[str]) -> list[str]:
+    """Get the canonical rotation representation from the sequence."""
+    state = get_identity_permutation(cube_size=1)
+    permutations = create_permutations(cube_size=1)
+
+    for rotation in rotations:
+        state = state[permutations[rotation]]
+
+    return CANONICAL_ROTATION_SEQUENCES[(state[0], state[1])]
+
+
 @dataclass(frozen=True)
 class MoveMeta:
-    """Meta information about moves, their permutations."""
+    """Meta information about moves and their permutations.
+
+    This class should capture all move-specific operations.
+    """
 
     cube_size: int
     permutations: dict[str, CubePermutation]
